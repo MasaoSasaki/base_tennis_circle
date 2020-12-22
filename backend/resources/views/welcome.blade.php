af8d778753c438d2ab76856b65ef322b12fb8fc3
佐々木正男 <masaosasaki@MacBook-Pro.local>
2020-12-22 17:20:28 +0900

    Fix css error

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('../layout')
 @section('content')
 <div class="welcome">
-  <button class="btn btn-primary">ログイン</button>
+  <a class="btn btn-primary" href="posts">ログイン</a>
 </div>
 @endsection
