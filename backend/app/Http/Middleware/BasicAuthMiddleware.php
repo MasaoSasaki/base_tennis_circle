8dfaac1f601cfd48089d560a3611e2db2fa88712
佐々木正男 <masaosasaki@MacBook-Pro.local>
2021-01-03 23:55:47 +0900

    Update BasicAuth pw change .env close #5

--- backend/app/Http/Middleware/BasicAuthMiddleware.php
+++ backend/app/Http/Middleware/BasicAuthMiddleware.php
@@ -18,8 +18,8 @@ class BasicAuthMiddleware
     {
       switch (true) {
         case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
-        // case $_SERVER['PHP_AUTH_USER'] !== '0000':
-        case $_SERVER['PHP_AUTH_PW']   !== '0000':
+        // case $_SERVER['PHP_AUTH_USER'] !== env('BASIC_AUTH_USER'):
+        case $_SERVER['PHP_AUTH_PW']   !== env('BASIC_AUTH_PW'):
           header('WWW-Authenticate: Basic realm="Enter username and password."');
           header('Content-Type: text/plain; charset=utf-8');
           die('このページを見るにはログインが必要です');
