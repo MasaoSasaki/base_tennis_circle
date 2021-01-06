b11f18464aeedab820d9692c7d7fe29f9c1bf589
佐々木正男 <masaosasaki@MacBook-Pro.local>
2021-01-06 12:39:42 +0900

    Add helper.php

---
+++ backend/app/helpers.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+if (! function_exists('hello')) {
+  /**
+   * @return string
+   */
+  function hello(): string
+  {
+    return 'Hello World.';
+  }
+
+  function getFolderName($album)
+  {
+    return preg_replace('/\s+|-|:|/', '', strval($album->created_at));
+  }
+
+  function getFileName($filePath, $folderName)
+  {
+    return str_replace([$folderName, '/'], '', $filePath);
+  }
+}
