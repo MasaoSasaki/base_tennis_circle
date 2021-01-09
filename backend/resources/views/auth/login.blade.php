c25032d31397a0cd085bca65aa5fce595bda2f56
佐々木正男 <masaosasaki@MacBook-Pro.local>
2021-01-09 12:15:31 +0900

    Update remove form values

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
               <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
               <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email" value="Administrator@example.com">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email">
                 @error('email')
                   <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -27,7 +27,7 @@
               <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
               <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"　value="Administrator">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                 @error('password')
                   <span class="invalid-feedback" role="alert">
